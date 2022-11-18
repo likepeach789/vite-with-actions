ffc2fe36a188e92dc258fcf35abc5a5d240341d5
choli <choli@ebay.com>
2022-11-18 20:15:32 +0800

    update: add a console.log

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import 'antd/dist/antd.css';
-
+console.log('how does it work')
 // import _ from 'lodash-es';
 // console.log('_', _)
 
